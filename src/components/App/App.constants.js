f8a9b6ca3b18758b7aff60dd3d7ac4bed37595e5
mirrorgo <cloudmirrorgo@163.com>
2022-04-13 11:05:04 +0800

    pwa

--- src/components/App/App.constants.js
+++ src/components/App/App.constants.js
@@ -50,11 +50,11 @@ const INITIAL_CONTENT = [
   {
     type: elementType.paragraph,
     children: [
-      { text: "Hello World! This is my paragraph inside a sample document." },
+      { text: "Hello World! test my slatejs" },
       { text: "Bold text.", bold: true, code: true },
       { text: "Italic text.", italic: true },
       { text: "Bold and underlined text.", bold: true, underline: true },
-      { text: "variableFoo", code: true },
+      { text: "support pwa", code: true },
     ],
   },
 ];
